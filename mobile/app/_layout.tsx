2390904f4c4afded1ad9d5586eef05ab4691e2fd
shrxprxyx <shripriya.sriram1@gmail.com>
2026-08-18 21:01:41 +0530

    [fix] : Fixed all dependency issues and basic mobile scaffold created using nativewind and react native

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import "../src/global.css";
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
